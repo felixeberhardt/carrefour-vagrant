c3b61646ef7bd76e177e34d5d95effeebac5c563
Andreas Grapentin <andreas@grapentin.org>
2020-04-02 19:37:18 +0200

    fixing package installs in bootstrap, extending root partition, using carrefour kernel config and branch 3.9

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,33 @@
 #!/usr/bin/env bash
 
-sudo apt install kernel-package fakeroot
-sudo apt install git
-git clone https://github.com/oaken-source/linux-replication.git
-cd linux-replication/
-git checkout replication-v3.12
-cp -v /boot/config-$(uname -r) .config
-make olddefconfig 
-make-kpkg clean
-fakeroot make-kpkg --initrd --revision=3.12.replication kernel_image
-sudo dpkg -i ../linux-image-3.16-subarchitecture_1.0.custom_i386.deb
+# extend root partition
+sudo apt-get install -y parted
+sudo parted -s /dev/vda resizepart 1 100%
+sudo resize2fs /dev/vda1
+
+# install required packages
+sudo apt-get install -y kernel-package fakeroot
+sudo apt-get install -y git
+
+# checkout kernel tree
+[ -d linux-replication ] || \
+    git clone https://github.com/oaken-source/linux-replication.git
+cd linux-replication/ || exit
+git checkout replication-v3.9
+
+# configure
+[ -f .config ] || (
+  cp -v config-bench .config
+  make olddefconfig
+)
+
+# build
+faketty () {
+    script -qefc "$(printf "%q " "$@")"
+}
+
+faketty make-kpkg clean
+faketty fakeroot make-kpkg --initrd --revision=3.9.replication kernel_image
+
+# install
+#sudo dpkg -i ../linux-image-3.16-subarchitecture_1.0.custom_i386.deb
